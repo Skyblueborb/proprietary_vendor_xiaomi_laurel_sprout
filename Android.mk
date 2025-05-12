c6bb4e9798642bbf1ab138d2aa7118223896a3da
Skyblueborb <tomaszborbely0710@gmail.com>
2025-05-12 16:06:47 +0200

    laurel_sprout: Import firmware from V12.0.26.0.RFQMIXM
    Change-Id: Ifd6cc33959f9c319ad5f5dd7e08fc62c0263f415

--- Android.mk
+++ Android.mk
@@ -3,3 +3,25 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),laurel_sprout)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,4f7efa8e1daaef2a1c21293f81b5897abcc5d04d)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,f5fa45cd5ddf0162aefb304dc574a0ce6bc06b8d)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,3bb7252efda5227628c405ca098c184fc99318b8)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,17706248814de99c98cb724a305093e834614178)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,78d2054e17632f72104c1537986781182bbebf57)
+$(call add-radio-file-sha1-checked,radio/dsp.img,3f276425dda8354677a1ec27df942b7a2266b398)
+$(call add-radio-file-sha1-checked,radio/hyp.img,c03b9d1eddae4522f8e386bf6624bbaaec0a2798)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,839b236de2a07687bc21cc01fde46549003b5dcd)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,05cfaed2484e5ef67a13281ab1597485871b2f2b)
+$(call add-radio-file-sha1-checked,radio/modem.img,e6dffef2f9a2555585572ec4fcb2bc08b1979e1c)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,c755ee17b2cf7037318c114b95f817f9ebbeb0da)
+$(call add-radio-file-sha1-checked,radio/rpm.img,2ebbc86da9a5209ada26cc7dc8976f4c32c2bd37)
+$(call add-radio-file-sha1-checked,radio/storsec.img,04cde583cfa75318443510ab77346e9414f31bee)
+$(call add-radio-file-sha1-checked,radio/tz.img,2c9c9a9914f17976200c2b21ea585a427d5d36e2)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,520ba9beb2b6ff2f2932fa5fa207b4441356d144)
+$(call add-radio-file-sha1-checked,radio/xbl.img,cd4f5a6482332066f3a3569370b70303c7547504)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,2f67186c51250d8c066f5573f07e89c2e5377ff8)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,22 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    bluetooth \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    dsp \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    qupfw \
+    rpm \
+    storsec \
+    tz \
+    uefisecapp \
+    xbl \
+    xbl_config
